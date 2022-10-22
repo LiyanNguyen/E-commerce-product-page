f1b8af92d2ee172b00e8907ba54fc7a784314993
LiyanNguyen <liyannguyenn@gmail.com>
2022-10-22 23:28:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let mainImage = document.querySelector("#mainImage");
 let lightBoxImage = document.querySelector("#lightBoxImage");
 let lightBox = document.querySelector(".lightBox");
 let closeLightBox = document.querySelector("#closeLightBox");
-let thumb1 = document.querySelector("#thumb1");
-let thumb2 = document.querySelector("#thumb2");
-let thumb3 = document.querySelector("#thumb3");
-let thumb4 = document.querySelector("#thumb4");
 let minus = document.querySelector("#minus");
 let plus = document.querySelector("#plus");
 let countDisplay = document.querySelector("#count");
@@ -13,11 +9,17 @@ let count = 0;
 let allThumbnails = document.querySelectorAll(".thumbnail");
 let currentMainImagePath = "images/image-product-1.jpg";
 
-let removeSelectedStateInAllThumbnails = () => {
-	allThumbnails.forEach((el) => {
-		el.classList.remove("selected");
+allThumbnails.forEach((el, index) => {
+	el.addEventListener("click", () => {
+		currentMainImagePath = `images/image-product-${index + 1}.jpg`;
+    mainImage.src = currentMainImagePath;
+    lightBoxImage.src = currentMainImagePath;
+    allThumbnails.forEach((thumbnails) => {
+      thumbnails.classList.remove("selected");
+    });
+    el.classList.add("selected");
 	})
-};
+});
 
 mainImage.onclick = () => {
 	lightBox.classList.add("active")
@@ -26,38 +28,6 @@ mainImage.onclick = () => {
 closeLightBox.onclick = () => {
 	lightBox.classList.remove("active")
 }
-
-thumb1.onclick = () => {
-	currentMainImagePath = "images/image-product-1.jpg";
-	mainImage.src = currentMainImagePath;
-	lightBoxImage.src = currentMainImagePath;
-	removeSelectedStateInAllThumbnails();
-	thumb1.classList.add("selected")
-};
-
-thumb2.onclick = () => {
-	currentMainImagePath = "images/image-product-2.jpg";
-	mainImage.src = currentMainImagePath;
-	lightBoxImage.src = currentMainImagePath;
-	removeSelectedStateInAllThumbnails();
-  thumb2.classList.add("selected");
-};
-
-thumb3.onclick = () => {
-	currentMainImagePath = "images/image-product-3.jpg";
-	mainImage.src = currentMainImagePath;
-  lightBoxImage.src = currentMainImagePath;
-	removeSelectedStateInAllThumbnails();
-  thumb3.classList.add("selected");
-};
-
-thumb4.onclick = () => {
-	currentMainImagePath = "images/image-product-4.jpg";
-	mainImage.src = currentMainImagePath;
-  lightBoxImage.src = currentMainImagePath;
-	removeSelectedStateInAllThumbnails();
-  thumb4.classList.add("selected");
-};
 
 minus.onclick = () => {
 	if (count > 0) {
