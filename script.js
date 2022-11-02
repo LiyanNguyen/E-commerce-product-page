d8efd734af6b60b80400e65350b8837e871079a4
LiyanNguyen <liyannguyenn@gmail.com>
2022-11-02 12:51:49 +0800

    Added Image Switcher

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ let mainImage = document.querySelector("#mainImage");
 let lightBoxImage = document.querySelector("#lightBoxImage");
 let lightBox = document.querySelector(".lightBox");
 let closeLightBox = document.querySelector("#closeLightBox");
+let lightBoxCircleLeft = document.querySelector(".lightBox .circle.left");
+let lightBoxCircleRight = document.querySelector(".lightBox .circle.right");
+let MobileCircleLeft = document.querySelector(".circle.left");
+let MobileCircleRight = document.querySelector(".circle.right");
 let minus = document.querySelector("#minus");
 let plus = document.querySelector("#plus");
 let countDisplay = document.querySelector("#count");
 let count = 0;
 let allThumbnails = document.querySelectorAll(".thumbnail");
 let currentMainImagePath = "images/image-product-1.jpg";
+let imageCount = 1;
 
 allThumbnails.forEach((el, index) => {
 	el.addEventListener("click", () => {
 		currentMainImagePath = `images/image-product-${index + 1}.jpg`;
-    mainImage.src = currentMainImagePath;
-    lightBoxImage.src = currentMainImagePath;
-    allThumbnails.forEach((thumbnails) => {
-      thumbnails.classList.remove("selected");
-    });
-    el.classList.add("selected");
+		mainImage.src = currentMainImagePath;
+		lightBoxImage.src = currentMainImagePath;
+		allThumbnails.forEach((thumbnails) => {
+			thumbnails.classList.remove("selected");
+		});
+		el.classList.add("selected");
 	})
 });
 
@@ -38,5 +43,41 @@ minus.onclick = () => {
 
 plus.onclick = () => {
 	count++;
-  countDisplay.innerText = count;
+	countDisplay.innerText = count;
 };
+
+let changeLightBoxImage = () => {
+	currentMainImagePath = `images/image-product-${imageCount}.jpg`;
+	mainImage.src = currentMainImagePath;
+	lightBoxImage.src = currentMainImagePath;
+	allThumbnails.forEach((thumbnails) => {
+		thumbnails.classList.remove("selected");
+	});
+	allThumbnails.forEach((el, index) => {
+		if (index == imageCount - 1) {
+			el.classList.add("selected");
+		}
+	});
+}
+
+let previousImage = () => {
+	if (imageCount == 1) {
+    imageCount = 5;
+  }
+  imageCount--;
+  changeLightBoxImage();
+}
+
+let nextImage = () => {
+	if (imageCount == 4) {
+    imageCount = 0;
+  }
+  imageCount++;
+  changeLightBoxImage();
+}
+
+lightBoxCircleLeft.addEventListener("click", previousImage);
+lightBoxCircleRight.addEventListener("click", nextImage);
+
+MobileCircleLeft.addEventListener("click", previousImage);
+MobileCircleRight.addEventListener("click", nextImage);
